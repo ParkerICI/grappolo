394f9f71dad5092798cc7c611e8c42181aa1386d
pfgherardini <federico.gherardini@gmail.com>
2018-04-04 12:42:18 -0700

    added some utility functions

--- R/io.R
+++ R/io.R
@@ -51,3 +51,54 @@ convert_fcs <- function(f, asinh.cofactor, transform.data = T, clip.at.zero = T,
     return(tab)
 }
 
+
+#' Get the columns that are common to a set of input tabular files
+#'
+#' @param files.list A vector of input file names. If these are text files, each file should be a tab-separated table,
+#'   with the first row representing column headers
+#' @param file.type The type of the files
+#' @return Returns a vector of column names that are present in all the files in \code{files.list}
+#' @export
+get_common_columns <- function(files.list, file.type = c("txt", "fcs")) {
+    l <- list()
+    file.type <- match.arg(file.type)
+
+    for(f in files.list) {
+        temp <- NULL
+
+        if(file.type == "txt")
+            temp <- read.table(f, header = T, sep = "\t", check.names = F, quote = "", nrows = 1)
+        else if(file.type == "fcs") {
+            fcs <- flowCore::read.FCS(f, which.lines = 1)
+            temp <- convert_fcs(fcs, asinh.cofactor = 1, transform.data = F, compensate = F)
+        }
+
+        l <- c(l, list(names(temp)))
+
+    }
+    return(Reduce(intersect, l))
+}
+
+#' Remove empty files
+#'
+#' This is an utility function to remove empty FCS files, which can cause problems with downstream analysis
+#'
+#' @param files.list The list of FCS files to check, files with number of events \code{<= events.threshold}
+#'   will be removed
+#' @param events.threshold A number. FCS files with number of events \code{<=} than this threshold will be removed
+#'
+#' @export
+remove_empty_files <- function(files.list, events.threshold = 0) {
+    for(f in files.list) {
+        fcs <- flowCore::read.FCS(f)
+        if(dim(fcs)["events"] <= events.threshold) {
+            message(sprintf("Removing %s", f))
+            file.remove(f)
+
+        }
+    }
+    return(invisible(NULL))
+}
+
+
+
