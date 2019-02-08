17d305827095ca712de0b2d8f8bf2e66f9bbefe2
pfgherardini <federico.gherardini@gmail.com>
2019-02-08 14:13:29 -0800

    fixed missing dplyr import

---
+++ R/grappolo.R
@@ -0,0 +1,7 @@
+#' grappolo: A package to cluster single-cell data and generate features that can be used for model building
+#'
+#' @docType package
+#' @name grappolo
+#' @importFrom dplyr %>%
+
+NULL
